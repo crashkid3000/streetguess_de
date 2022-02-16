bb512f1370e51377f00888ac77e640b9c799e9a7
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-02-16 22:26:00 +0100

    Added some javadoc

--- src/main/java/de/braack/streetguess_de/interpreter/literals/AdjectiveEndingLiteral.java
+++ src/main/java/de/braack/streetguess_de/interpreter/literals/AdjectiveEndingLiteral.java
@@ -1,5 +1,8 @@
 package de.braack.streetguess_de.interpreter.literals;
 
+/**
+ * Represents an adjective ending. Used as helper class for constructing all adjectives, but not used beyond that.
+ */
 public class AdjectiveEndingLiteral extends Literal {
     public AdjectiveEndingLiteral(String value) {
         super(value);
