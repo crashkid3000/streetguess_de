6d42ed5d755e2d32a7123b49795f221e3cfc2e8e
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-02-16 22:25:39 +0100

    Redid error handling, added some javadocs

--- src/main/java/de/braack/streetguess_de/interpreter/factory/XmlSourceFactory.java
+++ src/main/java/de/braack/streetguess_de/interpreter/factory/XmlSourceFactory.java
@@ -1,6 +1,6 @@
 package de.braack.streetguess_de.interpreter.factory;
 
-import de.braack.streetguess_de.interpreter.literals.Literal;
+import de.braack.streetguess_de.interpreter.exception.InternalConfigError;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -16,33 +16,66 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * A factory that creates instances of a given type by reading from an XML file. Implementing classes must specify
+ * an Xpath String <code>xpathString</code> that will be used for finding the instances. Additionally, the implementing
+ * classes must also specify how to extract the objects from the {@link NodeList}.
+ * @param <T> The type of the intances thata re to be created.
+ */
 public abstract class XmlSourceFactory<T> {
 
     protected String xpathString;
     protected File content;
 
+    /**
+     * Creates a new XmlSourceFactory
+     * @param content The reference to the XML file.
+     */
     public XmlSourceFactory(File content){
         this.content = content;
     }
 
-    public T[] createLiterals() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException {
+    /**
+     * Attempts to create the objects from the file provided in the constructor.
+     * @return All instances of the given type <code>T</code> that could be extracted from the provided file.
+     * @throws IOException If something goes wrong while attempting to access the file, including parsing errors.
+     */
+    public T[] createLiterals() throws IOException {
         NodeList nodes = getNodesFromDocument();
-        T[] retVal = getLiteralsFromNodeList(nodes);
+        T[] retVal = getInstancesFromNodeList(nodes);
         return retVal;
     }
 
-    private NodeList getNodesFromDocument() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException {
+    /**
+     * Reads and parses the provided XML file. Also creates a {@link NodeList} for the found XML bits and pieces.
+     * @return The <code>Nodelist</code> from the XML.
+     * @throws IOException If something goes wrong while attempting to access the file, including parsing errors.
+     */
+    private NodeList getNodesFromDocument() throws IOException {
         FileInputStream fis = new FileInputStream(content);
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = builderFactory.newDocumentBuilder();
-        Document xmlDocument = builder.parse(fis);
+        DocumentBuilder builder;
+        try {
+            builder = builderFactory.newDocumentBuilder();
+        }
+        catch (ParserConfigurationException pce){
+            throw new InternalConfigError(pce);
+        }
+        Document xmlDocument;
+        try {
+            xmlDocument = builder.parse(fis);
+        }
+        catch (SAXException se) {
+            throw new IOException("Could not parse XML", se);
+        }
         XPath xPath = XPathFactory.newInstance().newXPath();
-        return (NodeList) xPath.compile(xpathString).evaluate(xmlDocument, XPathConstants.NODESET);
+        try {
+            return (NodeList) xPath.compile(xpathString).evaluate(xmlDocument, XPathConstants.NODESET);
+        }
+        catch (XPathExpressionException xpee){
+            throw new InternalConfigError("Could not parse Xpath", xpee);
+        }
     }
 
-    protected abstract T[] getLiteralsFromNodeList(NodeList nodes);
-
-    protected String getXpathString(){
-        return xpathString;
-    }
+    protected abstract T[] getInstancesFromNodeList(NodeList nodes);
 }
