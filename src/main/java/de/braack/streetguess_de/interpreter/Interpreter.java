65f368608398332c9e3a395cb5aae810ad9adce6
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2022-02-16 22:25:50 +0100

    Added new String param

--- src/main/java/de/braack/streetguess_de/interpreter/Interpreter.java
+++ src/main/java/de/braack/streetguess_de/interpreter/Interpreter.java
@@ -1,6 +1,8 @@
 package de.braack.streetguess_de.interpreter;
 
+import java.io.IOException;
+
 public interface Interpreter {
 
-    String interpret();
+    String interpret(String street) throws IOException;
 }
